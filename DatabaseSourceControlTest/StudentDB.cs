37bd09d73557fef79e3665cbbb238029d3cb38b1
taylorjnickle <31074771+taylorjnickle@users.noreply.github.com>
2018-01-08 14:31:44 -0800

    Added StudentDB class with 2 incomplete methods

--- DatabaseSourceControlTest/StudentDB.cs
+++ DatabaseSourceControlTest/StudentDB.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace DatabaseSourceControlTest
 {
+    /// <summary>
+    /// Contains CRUD functionality for students
+    /// </summary>
     class StudentDB
     {
+        /// <summary>
+        /// Gets all students from the database
+        /// </summary>
+        /// <returns></returns>
+        public static List<String> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a student to the database
+        /// </summary>
+        public static void AddStudent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
